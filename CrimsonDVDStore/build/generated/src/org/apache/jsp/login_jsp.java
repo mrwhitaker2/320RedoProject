84e5477870e690eb9fcf6da9bab353b0d6ef7c88
wksullivan1 <wksullivan1@crimson.ua.edu>
2017-04-09 17:01:24 -0500

    Login Beginning

--- CrimsonDVDStore/build/generated/src/org/apache/jsp/login_jsp.java
+++ CrimsonDVDStore/build/generated/src/org/apache/jsp/login_jsp.java
@@ -11,20 +11,10 @@ public final class login_jsp extends org.apache.jasper.runtime.HttpJspBase
 
   private static java.util.List<String> _jspx_dependants;
 
-  private org.apache.jasper.runtime.TagHandlerPool _jspx_tagPool_c_out_value_nobody;
-
   private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
 
   public java.util.List<String> getDependants() {
     return _jspx_dependants;
-  }
-
-  public void _jspInit() {
-    _jspx_tagPool_c_out_value_nobody = org.apache.jasper.runtime.TagHandlerPool.getTagHandlerPool(getServletConfig());
-  }
-
-  public void _jspDestroy() {
-    _jspx_tagPool_c_out_value_nobody.release();
   }
 
   public void _jspService(HttpServletRequest request, HttpServletResponse response)
@@ -51,46 +41,54 @@ public final class login_jsp extends org.apache.jasper.runtime.HttpJspBase
       _jspx_out = out;
       _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
 
-      out.write("\n");
-      out.write("\n");
-      out.write("\n");
-      out.write("\n");
-      out.write("\n");
-      out.write("<!DOCTYPE html>\n");
-      out.write("<html>\n");
-      out.write("\n");
-      out.write("    <head>\n");
-      out.write("        <title>Login Page!</title>\n");
-      out.write("        <link rel=\"stylesheet\" type=\"text/css\" href=\"login.css\">\n");
-      out.write("        <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js\"></script>\n");
-      out.write("        <script src=\"https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js\"></script>\n");
-      out.write("\n");
-      out.write("        <script src=\"https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js\"></script>\n");
-      out.write("\n");
-      out.write("    </head>\n");
-      out.write("    <body>\n");
-      out.write("        <h1>Welcome to Crimson Video Store!</h1> <br>\n");
-      out.write("        <br>\n");
-      out.write("         <form method=\"POST\" action='FilmController' name=\"frmCustomerLogIn\">\n");
-      out.write("            Username : <input type=\"text\"  name=\"username\"\n");
-      out.write("                             value=\"");
-      if (_jspx_meth_c_out_0(_jspx_page_context))
-        return;
-      out.write("\" /> <br /> \n");
-      out.write("            Password : <input type=\"number\" required min=\"8\" name=\"password\"\n");
-      out.write("                             value=\"");
-      if (_jspx_meth_c_out_1(_jspx_page_context))
-        return;
-      out.write("\" /> <br />  <input\n");
-      out.write("                             type=\"submit\" value=\"Submit\" /> <br>\n");
-      out.write("            <br>\n");
-      out.write("            \n");
-      out.write("            <p><a href=\"FilmController?action=insert\">Create an Account</a></p>\n");
-      out.write("            <p><a href=\"FilmController?action=getemployee\">Manager Sign-In</a></p>\n");
-      out.write("            \n");
-      out.write("            \n");
-      out.write("    </body>\n");
-      out.write("</html>\n");
+      out.write("<!DOCTYPE html>\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("<head>\r\n");
+      out.write("    <title>Login Page!</title>\r\n");
+      out.write("    <link rel=\"stylesheet\" type=\"text/css\" href=\"login.css\">\r\n");
+      out.write("    <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js\"></script>\r\n");
+      out.write("    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js\"></script>\r\n");
+      out.write("    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js\"></script>\r\n");
+      out.write("\r\n");
+      out.write("</head>\r\n");
+      out.write("\r\n");
+      out.write("<div class=\"login-page\">        \r\n");
+      out.write("    <script type=\"text/javascript\">\r\n");
+      out.write("        function register() {\r\n");
+      out.write("            $('form').animate({height: \"toggle\", opacity: \"toggle\"}, \"slow\");\r\n");
+      out.write("        }\r\n");
+      out.write("        ;\r\n");
+      out.write("        function signIn() {\r\n");
+      out.write("            $('form').animate({height: \"toggle\", opacity: \"toggle\"}, \"slow\");\r\n");
+      out.write("        }\r\n");
+      out.write("        ;\r\n");
+      out.write("    </script>\r\n");
+      out.write("\r\n");
+      out.write("    <div class=\"form\">\r\n");
+      out.write("        <form class=\"register-form\">\r\n");
+      out.write("            <input type=\"radio\" name=\"store\" value=\"1\" checked>Store 1\r\n");
+      out.write("            <input type=\"radio\" name=\"store\" value=\"2\">Store 2\r\n");
+      out.write("            <input type=\"text\" name=\"fistName\" placeholder=\"First Name\" required/>\r\n");
+      out.write("            <input type=\"text\" name=\"lastName\" placeholder=\"Last Name\" required/>\r\n");
+      out.write("            <input type=\"email\" name=\"email\" placeholder=\"email address\" required/>\r\n");
+      out.write("            <input type=\"password\" name = \"password\" placeholder=\"password\" minlength=\"8\" pattern=\"(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,}\" title=\"Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters\" required/>\r\n");
+      out.write("            <button>create</button>\r\n");
+      out.write("            <p class=\"message\">Already registered? <a href=\"#\" onclick=\"signIn()\">Sign In</a></p>\r\n");
+      out.write("        </form>\r\n");
+      out.write("\r\n");
+      out.write("        <form class=\"login-form\">\r\n");
+      out.write("            <input type=\"email\" name = \"email\" autocomplete=\"on\" placeholder=\"email\" required />\r\n");
+      out.write("            <input type=\"password\" name =\"password\" placeholder=\"password\" minlength=\"8\" pattern=\"(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,}\" title=\"Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters\" required/>\r\n");
+      out.write("            <button>login</button>\r\n");
+      out.write("            <p class=\"message\">Not registered? <a href=\"#\" onclick=\"register()\">Create an account</a></p>\r\n");
+      out.write("            <p class=\"message\"> Manager or Staff?<a href=\"#\">Click Here</a></p>\r\n");
+      out.write("        </form>\r\n");
+      out.write("    </div>\r\n");
+      out.write("</div>\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
     } catch (Throwable t) {
       if (!(t instanceof SkipPageException)){
         out = _jspx_out;
@@ -102,41 +100,5 @@ public final class login_jsp extends org.apache.jasper.runtime.HttpJspBase
     } finally {
       _jspxFactory.releasePageContext(_jspx_page_context);
     }
-  }
-
-  private boolean _jspx_meth_c_out_0(PageContext _jspx_page_context)
-          throws Throwable {
-    PageContext pageContext = _jspx_page_context;
-    JspWriter out = _jspx_page_context.getOut();
-    //  c:out
-    org.apache.taglibs.standard.tag.rt.core.OutTag _jspx_th_c_out_0 = (org.apache.taglibs.standard.tag.rt.core.OutTag) _jspx_tagPool_c_out_value_nobody.get(org.apache.taglibs.standard.tag.rt.core.OutTag.class);
-    _jspx_th_c_out_0.setPageContext(_jspx_page_context);
-    _jspx_th_c_out_0.setParent(null);
-    _jspx_th_c_out_0.setValue((java.lang.Object) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${customer.username}", java.lang.Object.class, (PageContext)_jspx_page_context, null));
-    int _jspx_eval_c_out_0 = _jspx_th_c_out_0.doStartTag();
-    if (_jspx_th_c_out_0.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
-      _jspx_tagPool_c_out_value_nobody.reuse(_jspx_th_c_out_0);
-      return true;
-    }
-    _jspx_tagPool_c_out_value_nobody.reuse(_jspx_th_c_out_0);
-    return false;
-  }
-
-  private boolean _jspx_meth_c_out_1(PageContext _jspx_page_context)
-          throws Throwable {
-    PageContext pageContext = _jspx_page_context;
-    JspWriter out = _jspx_page_context.getOut();
-    //  c:out
-    org.apache.taglibs.standard.tag.rt.core.OutTag _jspx_th_c_out_1 = (org.apache.taglibs.standard.tag.rt.core.OutTag) _jspx_tagPool_c_out_value_nobody.get(org.apache.taglibs.standard.tag.rt.core.OutTag.class);
-    _jspx_th_c_out_1.setPageContext(_jspx_page_context);
-    _jspx_th_c_out_1.setParent(null);
-    _jspx_th_c_out_1.setValue((java.lang.Object) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${customer.password}", java.lang.Object.class, (PageContext)_jspx_page_context, null));
-    int _jspx_eval_c_out_1 = _jspx_th_c_out_1.doStartTag();
-    if (_jspx_th_c_out_1.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
-      _jspx_tagPool_c_out_value_nobody.reuse(_jspx_th_c_out_1);
-      return true;
-    }
-    _jspx_tagPool_c_out_value_nobody.reuse(_jspx_th_c_out_1);
-    return false;
   }
 }
--- CrimsonDVDStore/web/login.jsp
+++ CrimsonDVDStore/web/login.jsp
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
-
+<%@ page language="java" contentType="text/html; charset=EUC-KR" pageEncoding="EUC-KR"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
 <head>
     <title>Login Page!</title>
     <link rel="stylesheet" type="text/css" href="login.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 
 </head>
@@ -18,24 +19,38 @@
         ;
         function signIn() {
             $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
-        };
+        }
+        ;
     </script>
 
     <div class="form">
         <form class="register-form">
-            <input type="radio" name="store" value="1" checked>Store 1
-            <input type="radio" name="store" value="2">Store 2
-            <input type="text" name="fistName" placeholder="First Name" required/>
-            <input type="text" name="lastName" placeholder="Last Name" required/>
-            <input type="email" name="email" placeholder="email address" required/>
-            <input type="password" name = "password" placeholder="password" minlength="8" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required/>
+            <input type="radio" name="store" value="<c:out value="${customer.store}" />" checked>
+            Store 1
+            <input type="radio" name="store" value="<c:out value="${customer.store}" />">Store 2
+            <input type="text" name="fistName" placeholder="First Name" 
+                   value="<c:out value="${customer.firstName}" />" required/>
+            <input type="text" name="lastName" placeholder="Last Name" 
+                   value="<c:out value="${customer.lastName}" />" required/>
+            <input type="email" name="email" placeholder="email address" 
+                   value="<c:out value="${customer.email}" />"required/>
+            <input type="password" name = "password" placeholder="password" minlength="8" 
+                   pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" 
+                   title="Must contain at least one number and one uppercase and lowercase letter, 
+                   and at least 8 or more characters" value="<c:out value="${customer.password}" />"
+                   required/>
             <button>create</button>
             <p class="message">Already registered? <a href="#" onclick="signIn()">Sign In</a></p>
         </form>
 
         <form class="login-form">
-            <input type="email" name = "email" autocomplete="on" placeholder="email" required/>
-            <input type="password" name ="password" placeholder="password" minlength="8" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required/>
+            <input type="email" name = "email" autocomplete="on" placeholder="email" 
+                   value="<c:out value="${customer.email}" />" required />
+            <input type="password" name ="password" placeholder="password" minlength="8" 
+                   pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" 
+                   title="Must contain at least one number and one uppercase and lowercase letter, 
+                   and at least 8 or more characters"  value="<c:out value="${customer.password}" />"
+                   required/>
             <button>login</button>
             <p class="message">Not registered? <a href="#" onclick="register()">Create an account</a></p>
             <p class="message"> Manager or Staff?<a href="#">Click Here</a></p>
